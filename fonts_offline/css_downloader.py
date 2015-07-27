0eeae2b0cf7a27578497bba17a017cdabfeb7e13
Marco 'don' Kaulea <DonMarco42@gmail.com>
2015-07-27 21:13:55 +0200

    Add setup.py and remove useless code from package

--- fonts_offline/css_downloader.py
+++ fonts_offline/css_downloader.py
@@ -7,12 +7,6 @@
 # Marco 'don' Kaulea
 # ----------------------------------------------------------------------------
 
-'''Download fonts and create css to import them
-
-Usage:
-    fonts_offline [--ttf] [--font-path=<PATH>] <url>
-'''
-import docopt
 import pathlib as pl
 import re
 import requests
@@ -88,15 +82,6 @@ def replace_url(css, url, local_filename):
     return css.replace(url, local_filename)
 
 
-def main():
-    arguments = docopt.docopt(__doc__)
-    headers = {'User-agent': ('User-Agent: Mozilla/5.0 (X11;'
-                              ' Linux x86_64; rv:38.0) Gecko/20100101'
-                              'Firefox/38.0 Iceweasel/38.1.0')}
-    if '--ttf' in arguments and arguments['--ttf']:
-        headers = {}
-    font_path = arguments['--font-path'] if '--font-path' in arguments else ''
-
 def process_css_url(url, headers, font_path=''):
     result = requests.get(url, headers=headers)
     result.encoding = 'utf-8'
@@ -109,6 +94,3 @@ def process_css_url(url, headers, font_path=''):
             get_font_file(url, filename, headers)
             output_css = replace_url(output_css, url, filename)
     print(output_css)
-
-if __name__ == "__main__":
-    main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+long_description = """
+# fonts-offline
+Take fonts from google fonts and convert them for offline use.
+"""
+
+setup(
+    name='fonts-offline',
+    version='0.1.0',
+    description='Download fonts and create css to import them',
+    long_description=long_description,
+    author='Marco "don" Kaulea',
+    author_email='don@0xbeef.org',
+    url='https://github.com/Don42/fonts-offline',
+    packages=find_packages(),
+    install_requires=['docopt', 'requests'],
+    classifiers=[],
+    scripts=['fonts-offline.py'],
+)
